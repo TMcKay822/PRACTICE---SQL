3253dce885a8ff689b9ecd2fad8043f3b0a4aed5
Tiffany McKay <31213229+TMcKay822@users.noreply.github.com>
2019-11-28 03:13:05 -0500

    Update HackerRankSQLChallenge.sql

--- HackerRankSQLChallenge.sql
+++ HackerRankSQLChallenge.sql
@@ -2,7 +2,8 @@
 /* Query all columns for all American cities in CITY with populations larger than 100000. 
 The CountryCode for America is USA.
 */
-SELECT * FROM CITY
+SELECT * 
+FROM CITY
 WHERE COUNTRYCODE ='USA'
 AND POPULATION > 100000;
 
@@ -11,7 +12,8 @@ AND POPULATION > 100000;
 /* Query the names of all American cities in CITY with populations larger than 120000. 
 The CountryCode for America is USA.
 */ 
-SELECT NAME FROM CITY
+SELECT NAME 
+FROM CITY
 WHERE COUNTRYCODE = 'USA'
 AND POPULATION > 120000;
 
@@ -19,41 +21,58 @@ AND POPULATION > 120000;
 /* SELECT ALL */
 /* Query all columns (attributes) for every row in the CITY table.
 */
-SELECT * FROM CITY;
+SELECT * 
+FROM CITY;
 
 ------------------------------------------------------------------------------------------------------------------------
 /* SELECT by ID */
 /* Query all columns for a city in CITY with the ID 1661.
 */
-SELECT * FROM CITY 
+SELECT * 
+FROM CITY 
 WHERE ID =1661;
 
 ------------------------------------------------------------------------------------------------------------------------
 /*  Japanese Cities' Attributes */
 /* Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
 */
-SELECT * FROM CITY 
+SELECT * 
+FROM CITY 
 WHERE COUNTRYCODE = 'JPN';
 
 ------------------------------------------------------------------------------------------------------------------------
 /* Japanese Cities' Names */
 /* Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
 */ 
-SELECT NAME FROM CITY 
+SELECT NAME 
+FROM CITY 
 WHERE COUNTRYCODE = 'JPN';
 
 ------------------------------------------------------------------------------------------------------------------------
 /* Weather Observation Station 1 */
 /* Query a list of CITY and STATE from the STATION table.
 */ 
-SELECT CITY, STATE FROM STATION; 
+SELECT CITY, STATE 
+FROM STATION; 
+
+------------------------------------------------------------------------------------------------------------------------
+/* Weather Observation Station 2 */
+/* Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of  decimal places.
+The sum of all values in LONG_W rounded to a scale of  decimal places.
+*/
+
+
+
+
 
 ------------------------------------------------------------------------------------------------------------------------
 /* Weather Observation Station 3 */ 
 /* Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, 
 but must exclude duplicates from your answer.
 */
-SELECT DISTINCT CITY FROM STATION
+SELECT DISTINCT CITY 
+FROM STATION
 WHERE ID % 2=0
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -63,22 +82,31 @@ Query the two cities in STATION with the shortest and longest CITY names, as wel
 lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, 
 choose the one that comes first when ordered alphabetically.
 */
-SELECT top(1) CITY, LEN(CITY) FROM STATION  ORDER BY LEN(CITY), city ASC
-SELECT top(1) CITY, LEN(CITY) FROM STATION ORDER BY LEN(CITY) DESC, city ASC
+SELECT top(1) CITY, LEN(CITY) 
+FROM STATION  
+ORDER BY LEN(CITY), city ASC
+SELECT top(1) CITY, LEN(CITY) 
+FROM STATION 
+ORDER BY LEN(CITY) DESC, city ASC
 
 ------------------------------------------------------------------------------------------------------------------------
 /* Weather Observation Station 6 */
 /* Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
 Your result cannot contain duplicates.
 */
-SELECT DISTINCT(CITY) FROM STATION WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' 
-OR CITY LIKE 'U%' ORDER BY CITY ASC; 
+SELECT DISTINCT(CITY) 
+FROM STATION 
+WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' 
+OR CITY LIKE 'U%' 
+ORDER BY CITY ASC; 
 
 ------------------------------------------------------------------------------------------------------------------------
 /* Weather Observation Station 7 */
 /* Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 */
-SELECT DISTINCT(CITY) FROM STATION WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' 
+SELECT DISTINCT(CITY) 
+FROM STATION 
+WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' 
 OR CITY LIKE '%u';
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -87,7 +115,8 @@ OR CITY LIKE '%u';
 first and last characters. 
 Your result cannot contain duplicates.
 */
-SELECT DISTINCT City from STATION 
+SELECT DISTINCT City 
+FROM STATION 
 WHERE City LIKE '[aeiou]%[aeiou]'
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -95,24 +124,49 @@ WHERE City LIKE '[aeiou]%[aeiou]'
 /* Query the list of CITY names from STATION that do not start with vowels. 
 Your result cannot contain duplicates. 
 */
-SELECT DISTINCT City FROM STATION WHERE City LIKE '[^aeiou]%'
+SELECT DISTINCT City 
+FROM STATION 
+WHERE City LIKE '[^aeiou]%'
 
 ------------------------------------------------------------------------------------------------------------------------
 /* Weather Observation Station 10 */
 /* Query the list of CITY names from STATION that do not end with vowels. 
 Your result cannot contain duplicates.
 */
-SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE '%[^AEIOU]'
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY LIKE '%[^AEIOU]'
 
 ------------------------------------------------------------------------------------------------------------------------
 /* Weather Observation Station 11 */ 
 /* Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels.
 Your result cannot contain duplicates.
 */
-SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE '[^AEIOU]%' OR CITY LIKE '%[^AEIOU]'
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY LIKE '[^AEIOU]%' OR CITY LIKE '%[^AEIOU]'
 
+------------------------------------------------------------------------------------------------------------------------
+/* Weather Observation Station 12 */ 
+/* Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+Your result cannot contain duplicates.
+*/
 
+SELECT DISTINCT CITY 
+FROM STATION
+WHERE CITY LIKE '[^AEIOU]%' AND CITY LIKE '%[^AEIOU]'
 
+------------------------------------------------------------------------------------------------------------------------
+/* Higher Than 75 Marks */
+/* Query the Name of any student in STUDENTS who scored higher than 75 Marks. 
+Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), 
+secondary sort them by ascending ID.
+*/ 
+SELECT Name 
+FROM Students 
+WHERE Marks > 75
+ORDER BY RIGHT (Name,3), ID ASC
 
 
 
